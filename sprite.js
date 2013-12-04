b53f4e1694a8f6b09299f6d09c0e3f2f5a17029a
bryanmitchell <bryan.mitchell@upr.edu>
2013-12-03 20:32:44 -0800

    Create sprite.js

---
+++ sprite.js
@@ -0,0 +1,105 @@
+(function (lib, img, cjs) {
+
+var p; // shortcut to reference prototypes
+
+// stage content:
+(lib.nanito = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{spin:0});
+
+	// Sprite
+	this.instance = new lib.nano1();
+
+	this.instance_1 = new lib.nano2();
+
+	this.instance_2 = new lib.nano3();
+
+	this.instance_3 = new lib.nano4();
+
+	this.instance_4 = new lib.nano5();
+
+	this.instance_5 = new lib.nano6();
+
+	this.instance_6 = new lib.nano7();
+
+	this.instance_7 = new lib.nano8();
+
+	this.instance_8 = new lib.nano9();
+
+	this.instance_9 = new lib.nano10();
+
+	this.instance_10 = new lib.nano11();
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance}]}).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_2}]},1).to({state:[{t:this.instance_3}]},1).to({state:[{t:this.instance_4}]},1).to({state:[{t:this.instance_5}]},1).to({state:[{t:this.instance_6}]},1).to({state:[{t:this.instance_7}]},1).to({state:[{t:this.instance_8}]},1).to({state:[{t:this.instance_9}]},1).to({state:[{t:this.instance_10}]},1).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(0,0,200,216);
+
+
+// symbols:
+(lib.nano1 = function() {
+	this.initialize(img.nano1);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,200,216);
+
+
+(lib.nano10 = function() {
+	this.initialize(img.nano10);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,200,216);
+
+
+(lib.nano11 = function() {
+	this.initialize(img.nano11);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,200,216);
+
+
+(lib.nano2 = function() {
+	this.initialize(img.nano2);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,200,216);
+
+
+(lib.nano3 = function() {
+	this.initialize(img.nano3);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,200,216);
+
+
+(lib.nano4 = function() {
+	this.initialize(img.nano4);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,200,216);
+
+
+(lib.nano5 = function() {
+	this.initialize(img.nano5);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,200,216);
+
+
+(lib.nano6 = function() {
+	this.initialize(img.nano6);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,200,216);
+
+
+(lib.nano7 = function() {
+	this.initialize(img.nano7);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,200,216);
+
+
+(lib.nano8 = function() {
+	this.initialize(img.nano8);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,200,216);
+
+
+(lib.nano9 = function() {
+	this.initialize(img.nano9);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,200,216);
+
+})(lib = lib||{}, images = images||{}, createjs = createjs||{});
+var lib, images, createjs;
